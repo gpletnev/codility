4a3f8b03fff6ad3ceae70bca3ea8f4a85d9b1e9c
Grigory Pletnev <gpletnev@gmail.com>
2020-04-03 14:18:01 +0300

    141. Linked List Cycle

---
+++ src/main/java/leetcode/challenge/week1/HappyNumber.kt
@@ -0,0 +1,19 @@
+package leetcode.challenge.week1
+
+class HappyNumber {
+    fun isHappy(n: Int): Boolean {
+        if (n == 1 || n == 7) return true
+        return if (n < 10) false else isHappy(sumOfSquaresOfDigits(n))
+    }
+
+    private fun sumOfSquaresOfDigits(nn: Int): Int {
+        var n = nn
+        var sum = 0
+        while (n != 0) {
+            val digit = n % 10
+            sum += digit * digit
+            n /= 10
+        }
+        return sum
+    }
+}
---
+++ src/main/java/leetcode/easy/list/LinkedListCycle.kt
@@ -0,0 +1,19 @@
+package leetcode.easy.list
+
+// https://leetcode.com/problems/linked-list-cycle/
+object LinkedListCycle {
+    fun hasCycle(head: ListNode?): Boolean {
+        var slow: ListNode? = head
+        var fast: ListNode? = head
+        while (fast?.next != null) {
+            slow = slow?.next
+            fast = fast.next?.next
+            if (slow === fast) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+class ListNode(val next: ListNode?)
