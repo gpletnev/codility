8ae2cb104a08c9cd3edc7921c22ba01b1d335361
Grigory Pletnev <gpletnev@gmail.com>
2020-04-07 10:22:18 +0300

    Counting Elements

---
+++ src/main/java/leetcode/challenge/week1/CountingElements.kt
@@ -0,0 +1,14 @@
+package leetcode.challenge.week1
+
+object CountingElements {
+    fun countElements(arr: IntArray): Int {
+        var count = 0
+        val arrSet = arr.toHashSet()
+        for (i in arr) {
+            if (arrSet.contains(i + 1)) {
+                count++
+            }
+        }
+        return count
+    }
+}
--- src/test/java/leetcode/challenge/week1/Test.kt
+++ src/test/java/leetcode/challenge/week1/Test.kt
@@ -1,5 +1,6 @@
 package leetcode.challenge.week1
 
+import leetcode.challenge.week1.CountingElements.countElements
 import leetcode.challenge.week1.SingleNumber.singleNumber
 import org.junit.Assert
 import org.junit.Test
@@ -9,5 +10,13 @@ class Test {
     fun testSingleNumber() {
         Assert.assertEquals(1, singleNumber(listOf(2, 2, 1).toIntArray()))
         Assert.assertEquals(4, singleNumber(listOf(4, 1, 2, 1, 2).toIntArray()))
+    }
+
+    @Test
+    fun testCountElements() {
+        Assert.assertEquals(2, countElements(listOf(1,2,3).toIntArray()))
+        Assert.assertEquals(0, countElements(listOf(1,1,3,3,5,5,7,7).toIntArray()))
+        Assert.assertEquals(3, countElements(listOf(1,3,2,3,5,0).toIntArray()))
+        Assert.assertEquals(2, countElements(listOf(1,1,2,2).toIntArray()))
     }
 }
