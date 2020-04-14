f25676cc75f0bbed9afae6de637c1b4650da9e64
Grigory Pletnev <gpletnev@gmail.com>
2020-04-14 12:25:46 +0300

    fun stringShift(s: String, shift: Array<IntArray>): String {
            var final = shift.fold(0, { finalShift, it ->
                val direction = it[0]
                val amount = it[1]
                finalShift.plus(when (direction) {
                    0 -> -amount
                    1 -> amount
                    else -> 0
                })
            })
            final %= s.length
            return if (final == 0 || final == s.length) s else {
                if (final > 0)
                    s.substring(s.length - final) + s.substring(0, s.length - final)
                else
                    s.substring(-final) + s.substring(0, -final)
            }
        }

--- src/test/java/leetcode/challenge/week2/Test.kt
+++ src/test/java/leetcode/challenge/week2/Test.kt
@@ -3,6 +3,7 @@ package leetcode.challenge.week2
 import leetcode.challenge.week2.BackspaceStringCompare.backspaceCompare
 import leetcode.challenge.week2.ContiguousArray.findMaxLength
 import leetcode.challenge.week2.LastStoneWeight.lastStoneWeight
+import leetcode.challenge.week2.PerformStringShifts.stringShift
 import org.junit.Assert
 import org.junit.Test
 
@@ -36,5 +37,12 @@ class Test {
         Assert.assertEquals(2, findMaxLength(listOf(0, 1).toIntArray()))
         Assert.assertEquals(2, findMaxLength(listOf(0, 1, 0).toIntArray()))
         Assert.assertEquals(6, findMaxLength(listOf(0, 0, 1, 0, 0, 1, 1, 0).toIntArray()))
+    }
+
+    @Test
+    fun testStringShift() {
+        Assert.assertEquals("cab", stringShift("abc", listOf(listOf(0, 1).toIntArray(), listOf(1, 2).toIntArray()).toTypedArray()))
+        Assert.assertEquals("efgabcd", stringShift("abcdefg", listOf(listOf(1, 1).toIntArray(), listOf(1, 1).toIntArray(), listOf(0, 2).toIntArray(), listOf(1, 3).toIntArray()).toTypedArray()))
+        Assert.assertEquals("hcjwpdh", stringShift("wpdhhcj", listOf(listOf(0, 7).toIntArray(), listOf(1, 7).toIntArray(), listOf(1, 0).toIntArray(), listOf(1, 3).toIntArray(), listOf(0, 3).toIntArray(), listOf(0, 6).toIntArray(), listOf(1, 2).toIntArray()).toTypedArray()))
     }
 }
