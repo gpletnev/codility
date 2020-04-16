f0a3318750df9e297c7da00f00e707073d7da185
Grigory Pletnev <gpletnev@gmail.com>
2020-04-16 12:17:27 +0300

    678. Valid Parenthesis String

---
+++ src/main/java/leetcode/challenge/week3/ValidParenthesisString.kt
@@ -0,0 +1,19 @@
+package leetcode.challenge.week3
+
+// https://leetcode.com/problems/valid-parenthesis-string/
+object ValidParenthesisString {
+    fun checkValidString(s: String): Boolean {
+        // the lowest and highest possible number of open left brackets
+        // after processing the current character in the string
+        var lowest = 0
+        var highest = 0
+        for (c in s.toCharArray()) {
+            lowest += if (c == '(') 1 else -1
+            highest += if (c != ')') 1 else -1
+            if (highest < 0) break
+            lowest = lowest.coerceAtLeast(0)
+        }
+        return lowest == 0
+    }
+}
+
--- src/test/java/leetcode/challenge/week3/Test.kt
+++ src/test/java/leetcode/challenge/week3/Test.kt
@@ -1,6 +1,7 @@
 package leetcode.challenge.week3
 
 import leetcode.challenge.week3.ProductOfArrayExceptSelf.productExceptSelf
+import leetcode.challenge.week3.ValidParenthesisString.checkValidString
 import org.junit.Assert
 import org.junit.Test
 
@@ -10,5 +11,15 @@ class Test {
     fun testProductExceptSelf() {
         Assert.assertArrayEquals(listOf(24, 12, 8, 6).toIntArray(), productExceptSelf(listOf(1, 2, 3, 4).toIntArray()))
         Assert.assertArrayEquals(listOf(0, 0).toIntArray(), productExceptSelf(listOf(0, 0).toIntArray()))
+    }
+
+    @Test
+    fun testCheckValidString() {
+        Assert.assertEquals(true, checkValidString("()"))
+        Assert.assertEquals(false, checkValidString(")("))
+        Assert.assertEquals(false, checkValidString("(("))
+        Assert.assertEquals(true, checkValidString("(*)"))
+        Assert.assertEquals(true, checkValidString("((*)"))
+        Assert.assertEquals(true, checkValidString("(*))"))
     }
 }
