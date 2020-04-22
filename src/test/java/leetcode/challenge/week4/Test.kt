91deffdbb3327f556a1c198b03cb42f9a3d4ff59
Grigory Pletnev <gpletnev@gmail.com>
2020-04-22 11:50:28 +0300

    560. Subarray Sum Equals K

---
+++ src/test/java/leetcode/challenge/week4/Test.kt
@@ -0,0 +1,12 @@
+package leetcode.challenge.week4
+
+import org.junit.Assert
+import org.junit.Test
+
+class Test {
+
+    @Test
+    fun testSubarraySumEqualsK() {
+        Assert.assertEquals(2, SubarraySumEqualsK.subarraySum(intArrayOf(1, 1, 1), 2))
+    }
+}
