cfeb7f1e1c612fb1e0e7440d25f6ba0d5e14eb23
Grigory Pletnev <gpletnev@gmail.com>
2020-04-24 14:45:59 +0300

    146. LRU Cache

--- src/test/java/leetcode/challenge/week4/Test.kt
+++ src/test/java/leetcode/challenge/week4/Test.kt
@@ -15,4 +15,18 @@ class Test {
         Assert.assertEquals(4, BitwiseANDOfNumbersRange.rangeBitwiseAnd(5, 7))
         Assert.assertEquals(0, BitwiseANDOfNumbersRange.rangeBitwiseAnd(0, 1))
     }
+
+    @Test
+    fun testLRUCache() {
+        val cache = LRUCache(2)
+        cache.put(1, 1)
+        cache.put(2, 2)
+        Assert.assertEquals(1, cache.get(1))
+        cache.put(3, 3)
+        Assert.assertEquals(-1, cache.get(2))
+        cache.put(4, 4)
+        Assert.assertEquals(-1, cache.get(1))
+        Assert.assertEquals(3, cache.get(3))
+        Assert.assertEquals(4, cache.get(4))
+    }
 }
