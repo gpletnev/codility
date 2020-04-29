c8f979c9d7c78ea55f048d9a5b2a8ee71bb4c55a
Grigory Pletnev <gpletnev@gmail.com>
2020-04-29 15:20:49 +0300

    124. Binary Tree Maximum Path Sum

---
+++ src/main/java/leetcode/challenge/week5/BinaryTreeMaximumPathSum.kt
@@ -0,0 +1,32 @@
+package leetcode.challenge.week5
+
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+object BinaryTreeMaximumPathSum {
+
+    private var maximumPathSum: Int? = null
+
+    fun maxPathSum(root: TreeNode?): Int {
+        maxPathDeep(root)
+        return maximumPathSum!!
+    }
+
+    private fun maxPathDeep(root: TreeNode?): Int {
+        if (root == null) return 0
+
+        val leftPathSum = if (root.left != null) maxPathDeep(root.left) else 0
+        val rightPathSum = if (root.right != null) maxPathDeep(root.right) else 0
+
+        val leftPathRootSum = leftPathSum + root.`val`
+        val rightPathRootSum = rightPathSum + root.`val`
+        val rootTreeSum = leftPathSum + rightPathSum + root.`val`
+
+        val rootMaximum = maxOf(maxOf(leftPathRootSum, rightPathRootSum, rootTreeSum), root.`val`)
+        maximumPathSum = maximumPathSum?.let { maxOf(it, rootMaximum) } ?: rootMaximum
+        return maxOf(leftPathRootSum, rightPathRootSum, root.`val`)
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
---
+++ src/test/java/leetcode/challenge/week5/Test.kt
@@ -0,0 +1,53 @@
+package leetcode.challenge.week5
+
+import org.junit.Assert
+import org.junit.Test
+
+class Test {
+
+    @Test
+    fun testBinaryTreeMaximumPathSum() {
+        var root = TreeNode(2)
+        root.left = TreeNode(-1)
+        Assert.assertEquals(2, BinaryTreeMaximumPathSum.maxPathSum(root))
+
+        root = TreeNode(1)
+        root.left = TreeNode(-2)
+        root.right = TreeNode(3)
+        Assert.assertEquals(4, BinaryTreeMaximumPathSum.maxPathSum(root))
+
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        Assert.assertEquals(6, BinaryTreeMaximumPathSum.maxPathSum(root))
+
+        root = TreeNode(-10)
+        root.left = TreeNode(9)
+        root.right = TreeNode(20)
+        root.right!!.left = TreeNode(15)
+        root.right!!.right = TreeNode(7)
+        Assert.assertEquals(42, BinaryTreeMaximumPathSum.maxPathSum(root))
+
+        root = TreeNode(9)
+        root.left = TreeNode(6)
+        root.right = TreeNode(-3)
+        root.right!!.left = TreeNode(-6)
+        root.right!!.right = TreeNode(2)
+        root.right!!.right!!.left = TreeNode(2)
+        root.right!!.right!!.left!!.left = TreeNode(-6)
+        root.right!!.right!!.left!!.right = TreeNode(-6)
+        root.right!!.right!!.left!!.left!!.left = TreeNode(-6)
+        Assert.assertEquals(16, BinaryTreeMaximumPathSum.maxPathSum(root))
+
+        root = TreeNode(5)
+        root.left = TreeNode(4)
+        root.right = TreeNode(8)
+        root.left!!.left = TreeNode(11)
+        root.left!!.left!!.left = TreeNode(7)
+        root.left!!.left!!.right = TreeNode(2)
+        root.right!!.left = TreeNode(13)
+        root.right!!.right = TreeNode(4)
+        root.right!!.right!!.right = TreeNode(1)
+        Assert.assertEquals(48, BinaryTreeMaximumPathSum.maxPathSum(root))
+    }
+}
