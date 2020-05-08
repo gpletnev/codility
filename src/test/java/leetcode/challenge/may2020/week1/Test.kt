85d114e14d7d4808142a46e7a7dd44575a392f14
Grigory Pletnev <gpletnev@gmail.com>
2020-05-08 14:11:54 +0300

    1232. Check If It Is a Straight Line

--- src/test/java/leetcode/challenge/may2020/week1/Test.kt
+++ src/test/java/leetcode/challenge/may2020/week1/Test.kt
@@ -1,5 +1,6 @@
 package leetcode.challenge.may2020.week1
 
+import leetcode.challenge.may2020.week2.CheckIfItIsAStraightLine
 import org.junit.Assert
 import org.junit.Test
 
@@ -16,5 +17,12 @@ class Test {
         Assert.assertEquals(false, CousinsInBinaryTree.isCousins(root, 4, 3))
         Assert.assertEquals(true, CousinsInBinaryTree.isCousins(root, 4, 5))
         Assert.assertEquals(false, CousinsInBinaryTree.isCousins(root, 2, 3))
+    }
+
+    @Test
+    fun testCheckIfItIsAStraightLine() {
+        Assert.assertEquals(true, CheckIfItIsAStraightLine.checkStraightLine(arrayOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(3, 4), intArrayOf(4, 5), intArrayOf(5, 6), intArrayOf(6, 7))))
+        Assert.assertEquals(false, CheckIfItIsAStraightLine.checkStraightLine(arrayOf(intArrayOf(1, 1), intArrayOf(2, 2), intArrayOf(3, 4), intArrayOf(4, 5), intArrayOf(5, 6), intArrayOf(7, 7))))
+        Assert.assertEquals(true, CheckIfItIsAStraightLine.checkStraightLine(arrayOf(intArrayOf(1, 1), intArrayOf(1, 2), intArrayOf(1, 4))))
     }
 }
