14edea47dede269939f8178436a900c1f24b9570
Grigory Pletnev <gpletnev@gmail.com>
2020-05-15 13:48:29 +0300

    53. Maximum Subarray

--- src/main/java/leetcode/challenge/april2020/week1/MaximumSubarray.kt
+++ src/main/java/leetcode/challenge/april2020/week1/MaximumSubarray.kt
@@ -1,9 +1,20 @@
 package leetcode.challenge.april2020.week1
 
-// https://leetcode.com/problems/maximum-subarray/submissions/
+import java.lang.Integer.max
+
+// https://leetcode.com/problems/maximum-subarray/
 object MaximumSubarray {
     fun maxSubArray(nums: IntArray): Int {
-        return maxSubArraySum(nums, 0, nums.lastIndex)
+        var localMax = 0
+        var globalMax = Int.MIN_VALUE
+
+        for (n in nums) {
+            localMax = max(n, n + localMax)
+            if (localMax > globalMax)
+                globalMax = localMax
+        }
+        return globalMax
+        //return maxSubArraySum(nums, 0, nums.lastIndex)
     }
 
     private fun maxSubArraySum(nums: IntArray, l: Int, r: Int): Int {
--- src/test/java/leetcode/challenge/april2020/week1/Test.kt
+++ src/test/java/leetcode/challenge/april2020/week1/Test.kt
@@ -13,10 +13,15 @@ class Test {
     }
 
     @Test
+    fun testMaximumSubarray() {
+        Assert.assertEquals(6, MaximumSubarray.maxSubArray(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)))
+    }
+
+    @Test
     fun testCountElements() {
-        Assert.assertEquals(2, countElements(listOf(1,2,3).toIntArray()))
-        Assert.assertEquals(0, countElements(listOf(1,1,3,3,5,5,7,7).toIntArray()))
-        Assert.assertEquals(3, countElements(listOf(1,3,2,3,5,0).toIntArray()))
-        Assert.assertEquals(2, countElements(listOf(1,1,2,2).toIntArray()))
+        Assert.assertEquals(2, countElements(listOf(1, 2, 3).toIntArray()))
+        Assert.assertEquals(0, countElements(listOf(1, 1, 3, 3, 5, 5, 7, 7).toIntArray()))
+        Assert.assertEquals(3, countElements(listOf(1, 3, 2, 3, 5, 0).toIntArray()))
+        Assert.assertEquals(2, countElements(listOf(1, 1, 2, 2).toIntArray()))
     }
 }
