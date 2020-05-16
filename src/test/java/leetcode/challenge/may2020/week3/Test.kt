0820323a71e60493fc484c1e8d06c767efb935a7
Grigory Pletnev <gpletnev@gmail.com>
2020-05-16 13:00:33 +0300

    328. Odd Even Linked List

--- src/test/java/leetcode/challenge/may2020/week3/Test.kt
+++ src/test/java/leetcode/challenge/may2020/week3/Test.kt
@@ -12,4 +12,20 @@ class Test {
         Assert.assertEquals(3, MaximumSumCircularSubarray.maxSubarraySumCircular(intArrayOf(3, -2, 2, -3)))
         Assert.assertEquals(-1, MaximumSumCircularSubarray.maxSubarraySumCircular(intArrayOf(-2, -3, -1)))
     }
+
+    @Test
+    fun testOddEvenLinkedList() {
+        Assert.assertEquals("null", OddEvenLinkedList.oddEvenList(null).toString())
+        val head = ListNode(1)
+        Assert.assertEquals("1 NULL", OddEvenLinkedList.oddEvenList(head).toString())
+        head.next = ListNode(2)
+        Assert.assertEquals("1 2 NULL", OddEvenLinkedList.oddEvenList(head).toString())
+        head.next?.next = ListNode(3)
+        Assert.assertEquals("1 3 2 NULL", OddEvenLinkedList.oddEvenList(head).toString())
+        head.next?.next?.next = ListNode(4)
+        Assert.assertEquals("1 3 2 4 NULL", OddEvenLinkedList.oddEvenList(head).toString())
+        head.next?.next?.next?.next = ListNode(5)
+        Assert.assertEquals("1 3 5 2 4 NULL", OddEvenLinkedList.oddEvenList(head).toString())
+    }
+
 }
