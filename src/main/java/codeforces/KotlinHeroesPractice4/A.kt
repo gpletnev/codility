0e6ad94fea75508920630c8ba84d7a808227343c
Grigory Pletnev <gpletnev@gmail.com>
2020-05-25 14:53:22 +0300

    1035. Uncrossed Lines

---
+++ src/main/java/codeforces/KotlinHeroesPractice4/A.kt
@@ -0,0 +1,16 @@
+package codeforces.KotlinHeroesPractice4
+
+/*
+fun readString() = readLine()!!
+
+fun readInt() = readString().toInt()
+fun readLong() = readString().toLong()
+
+fun readIntArray() = readString().split(" ").map { it.toInt() }
+fun readLongArray() = readString().split(" ").map { it.toLong() }
+
+fun main() {
+    repeat(readInt()) {
+        println(readIntArray().sum())
+    }
+}*/
--- src/test/java/leetcode/challenge/may2020/week4/Test.kt
+++ src/test/java/leetcode/challenge/may2020/week4/Test.kt
@@ -10,4 +10,11 @@ class Test {
         Assert.assertTrue(SortCharactersByFrequency.frequencySort("cccaaa") in listOf("cccaaa", "aaaccc"))
         Assert.assertTrue(SortCharactersByFrequency.frequencySort("Aabb") in listOf("bbAa", "bbaA"))
     }
+
+    @Test
+    fun testUncrossedLines() {
+        Assert.assertEquals(2, UncrossedLines.maxUncrossedLines(intArrayOf(1, 4, 2), intArrayOf(1, 2, 4)))
+        Assert.assertEquals(3, UncrossedLines.maxUncrossedLines(intArrayOf(2, 5, 1, 2, 5), intArrayOf(10, 5, 2, 1, 5, 2)))
+        Assert.assertEquals(2, UncrossedLines.maxUncrossedLines(intArrayOf(1, 3, 7, 1, 7, 5), intArrayOf(1, 9, 2, 5, 1)))
+    }
 }
