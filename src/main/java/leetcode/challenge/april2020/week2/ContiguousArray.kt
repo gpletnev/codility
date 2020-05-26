166590cafb002c3af20ea5398f86f0add8c46220
Grigory Pletnev <gpletnev@gmail.com>
2020-05-26 13:26:25 +0300

    525. Contiguous Array

--- src/main/java/leetcode/challenge/april2020/week2/ContiguousArray.kt
+++ src/main/java/leetcode/challenge/april2020/week2/ContiguousArray.kt
@@ -13,11 +13,13 @@ object ContiguousArray {
             count += if (nums[i] == 1) 1 else -1
 
             // Array from index 0 to i contains equal number of 0's and 1's
-            if (count == 0) maxlen = maxOf(maxlen, i + 1)
+            if (count == 0)
+                maxlen = maxOf(maxlen, i + 1)
 
             if (map.containsKey(count))
                 maxlen = maxOf(maxlen, i - map[count]!!)
-            else map[count] = i
+            else
+                map[count] = i
         }
 
         return maxlen
