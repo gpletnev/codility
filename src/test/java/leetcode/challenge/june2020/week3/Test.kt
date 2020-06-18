86de486658302e0fca742264eea389ded64b63f7
Grigory Pletnev <gpletnev@gmail.com>
2020-06-18 22:12:07 +0300

    275. H-Index II

--- src/test/java/leetcode/challenge/june2020/week3/Test.kt
+++ src/test/java/leetcode/challenge/june2020/week3/Test.kt
@@ -21,4 +21,9 @@ class Test {
 
     fun String.toBoard(columns: Int) = this.chunked(columns).map { it.toCharArray() }.toTypedArray()
     fun BoardToString(board: Array<CharArray>): String = board.joinToString("") { it.joinToString("") }
+
+    @Test
+    fun testH_IndexII() {
+        Assert.assertEquals(3, H_IndexII.hIndex(intArrayOf(0, 1, 3, 5, 6)))
+    }
 }
