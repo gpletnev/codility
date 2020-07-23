c69f0849013b7c8e5d19a38df75b18e1aef9adfc
Grigory Pletnev <gpletnev@gmail.com>
2020-07-23 15:20:29 +0300

    260. Single Number III

--- src/test/java/leetcode/challenge/july2020/week4/Test.kt
+++ src/test/java/leetcode/challenge/july2020/week4/Test.kt
@@ -12,4 +12,9 @@ class Test {
         val head = BinaryTree(listOf(3, 9, 20, null, null, 15, 7)).head
         Assert.assertEquals(listOf(listOf(3), listOf(20, 9), listOf(15, 7)), zigzagLevelOrder(head))
     }
+
+    @Test
+    fun testSingleNumberIII() {
+        Assert.assertArrayEquals(intArrayOf(3, 5), singleNumber(intArrayOf(1, 2, 1, 3, 2, 5)))
+    }
 }
