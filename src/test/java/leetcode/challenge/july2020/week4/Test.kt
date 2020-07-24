0661d557fd4152841a8a15382fb699b2f8d5c9eb
Grigory Pletnev <gpletnev@gmail.com>
2020-07-24 17:10:25 +0300

    797. All Paths From Source to Target

--- src/test/java/leetcode/challenge/july2020/week4/Test.kt
+++ src/test/java/leetcode/challenge/july2020/week4/Test.kt
@@ -4,6 +4,7 @@ import leetcode.BinaryTree
 import org.junit.Assert
 import org.junit.Test
 import java.util.*
+import kotlin.collections.ArrayList
 
 class Test {
     @Test
@@ -16,5 +17,12 @@ class Test {
     @Test
     fun testSingleNumberIII() {
         Assert.assertArrayEquals(intArrayOf(3, 5), singleNumber(intArrayOf(1, 2, 1, 3, 2, 5)))
+    }
+
+    @Test
+    fun testAllPathsSourceTarget() {
+        Assert.assertEquals(ArrayList<ArrayList<Int>>(), allPathsSourceTarget(arrayOf(intArrayOf(), intArrayOf())))
+        Assert.assertEquals(listOf(listOf(0, 1)), allPathsSourceTarget(arrayOf(intArrayOf(1), intArrayOf())))
+        Assert.assertEquals(listOf(listOf(0, 1, 3), listOf(0, 2, 3)), allPathsSourceTarget(arrayOf(intArrayOf(1, 2), intArrayOf(3), intArrayOf(3), intArrayOf())))
     }
 }
