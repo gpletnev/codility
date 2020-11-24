780b30db3a95633797372d9944ee030508e9b6a0
Grigory Pletnev <gpletnev@gmail.com>
2020-11-24 12:22:55 +0300

    227. Basic Calculator II

---
+++ src/main/java/leetcode/challenge/2020/11/week4/Basic Calculator II.kt
@@ -0,0 +1,37 @@
+package leetcode.challenge.`2020`.`11`.week4
+
+// https://leetcode.com/problems/basic-calculator-ii/
+fun calculate(s: String): Int {
+    if (s.isEmpty()) return 0
+
+    var currentNumber = 0
+    var lastNumber = 0
+    var result = 0
+    var operation = '+'
+
+    val iterator = s.iterator()
+    while (iterator.hasNext()) {
+        val currentChar = iterator.nextChar()
+        if (Character.isDigit(currentChar)) {
+            currentNumber = currentNumber * 10 + (currentChar - '0')
+        }
+        if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || !iterator.hasNext()) {
+            when (operation) {
+                '+', '-' -> {
+                    result += lastNumber
+                    lastNumber = if (operation == '+') currentNumber else -currentNumber
+                }
+                '*' -> {
+                    lastNumber *= currentNumber
+                }
+                '/' -> {
+                    lastNumber /= currentNumber
+                }
+            }
+            operation = currentChar
+            currentNumber = 0
+        }
+    }
+    result += lastNumber
+    return result
+}
--- src/test/java/leetcode/challenge/2020/11/week4/Test.kt
+++ src/test/java/leetcode/challenge/2020/11/week4/Test.kt
@@ -13,4 +13,11 @@ class Test {
         root = BinaryTree(listOf(3, 4, 5, 1, 3, null, 1)).head
         Assert.assertEquals(9, rob(root))
     }
+
+    @Test
+    fun calculateTest() {
+        Assert.assertEquals(7, calculate("3+2*2"))
+        Assert.assertEquals(1, calculate(" 3/2"))
+        Assert.assertEquals(5, calculate("3+5 / 2"))
+    }
 }
