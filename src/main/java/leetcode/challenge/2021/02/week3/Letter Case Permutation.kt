5c9e834d45f8e839d9e9809b35de5ad9c7133ed2
Grigory Pletnev <gpletnev@gmail.com>
2021-02-16 14:39:37 +0300

    784. Letter Case Permutation

---
+++ src/main/java/leetcode/challenge/2021/02/week3/Letter Case Permutation.kt
@@ -0,0 +1,28 @@
+package leetcode.challenge.`2021`.`02`.week3
+
+import kotlin.math.pow
+
+// https://leetcode.com/problems/letter-case-permutation/
+fun letterCasePermutation(S: String): List<String> {
+    val sChars = S.toCharArray()
+    val letterIndexes = mutableListOf<Int>()
+    for (i in sChars.indices) {
+        if (sChars[i].isLetter()) letterIndexes.add(i)
+    }
+
+    val bitCount = letterIndexes.size
+    val numOfPermutations = 2.toDouble().pow(bitCount).toInt()
+
+    val list = mutableListOf<String>()
+    for (i in 0 until numOfPermutations) {
+        val bitString = String.format("%12s", i.toString(radix = 2)).replace(' ', '0').reversed()
+        val perm = sChars.copyOf()
+        for (j in 0 until bitCount) {
+            val index = letterIndexes[j]
+            val bit = bitString[j]
+            perm[index] = if (bit == '1') sChars[index].toUpperCase() else sChars[index].toLowerCase()
+        }
+        list.add(String(perm))
+    }
+    return list
+}
--- src/test/java/leetcode/challenge/2021/02/week3/Test.kt
+++ src/test/java/leetcode/challenge/2021/02/week3/Test.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class Test {
     @Test
     fun kWeakestRowsTest() {
-/*        Assert.assertArrayEquals(
+        Assert.assertArrayEquals(
             intArrayOf(0, 2),
             kWeakestRows(
                 arrayOf(
@@ -17,7 +17,7 @@ class Test {
                 ),
                 2
             )
-        )*/
+        )
 
         Assert.assertArrayEquals(
             intArrayOf(2, 0, 3, 1),
@@ -32,5 +32,13 @@ class Test {
                 4
             )
         )
+    }
+
+    @Test
+    fun letterCasePermutationTest() {
+        Assert.assertEquals(listOf("a1b2", "A1b2", "a1B2", "A1B2"), letterCasePermutation("a1b2"))
+        Assert.assertEquals(listOf("3z4", "3Z4"), letterCasePermutation("3z4"))
+        Assert.assertEquals(listOf("12345"), letterCasePermutation("12345"))
+        Assert.assertEquals(listOf("0"), letterCasePermutation("0"))
     }
 }
