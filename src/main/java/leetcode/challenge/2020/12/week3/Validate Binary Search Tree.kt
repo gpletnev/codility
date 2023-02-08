862940b95385a93aeb357ecf48f8787ecebef5f4
gpletnev <gpletnev@gmail.com>
2023-02-08 13:17:15 +0300

    98. Validate Binary Search Tree

--- src/main/java/leetcode/challenge/2020/12/week3/Validate Binary Search Tree.kt
+++ src/main/java/leetcode/challenge/2020/12/week3/Validate Binary Search Tree.kt
@@ -1,10 +1,28 @@
 package leetcode.challenge.`2020`.`12`.week3
 
 import leetcode.TreeNode
+import java.util.*
 
 // https://leetcode.com/problems/validate-binary-search-tree/
 fun isValidBST(root: TreeNode?): Boolean {
-    return isValidBST(root, null, null)
+
+    if (root == null) return true
+    var node = root
+
+    val stack: Stack<TreeNode> = Stack()
+    var pre: TreeNode? = null
+    while (node != null || stack.isNotEmpty()) {
+        while (node != null) {
+            stack.push(node)
+            node = node.left
+        }
+        node = stack.pop()
+        if (pre != null && node.`val` <= pre.`val`) return false
+        pre = node
+        node = node.right
+    }
+    return true
+//    return isValidBST(root, null, null)
 }
 
 fun isValidBST(root: TreeNode?, min: Int?, max: Int?): Boolean {
