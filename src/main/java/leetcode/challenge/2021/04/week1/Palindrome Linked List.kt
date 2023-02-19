33e8263bcff69ce4ee4544bce05022e01b4d9201
gpletnev <gpletnev@gmail.com>
2023-02-19 12:55:09 +0300

    234. Palindrome Linked List

--- src/main/java/leetcode/challenge/2021/04/week1/Palindrome Linked List.kt
+++ src/main/java/leetcode/challenge/2021/04/week1/Palindrome Linked List.kt
@@ -4,7 +4,7 @@ import leetcode.ListNode
 
 // https://leetcode.com/problems/palindrome-linked-list/
 
-var isPalindrome = true
+/*var isPalindrome = true
 
 fun isPalindrome(head: ListNode?): Boolean {
     isPalindrome = true
@@ -20,4 +20,49 @@ fun isPalindrome(slow: ListNode, fast: ListNode): ListNode? {
         isPalindrome = false
     }
     return mySlow.next
+}*/
+
+fun isPalindrome(head: ListNode?): Boolean {
+    if (head == null) return true
+
+    // Find the end of first half and reverse second half.
+    val firstHalfEnd = endOfFirstHalf(head)
+    val secondHalfStart = reverseList(firstHalfEnd!!.next)
+
+    // Check whether or not there is a palindrome.
+    var p1 = head
+    var p2 = secondHalfStart
+    var result = true
+    while (result && p2 != null) {
+        if (p1!!.`val` != p2.`val`) result = false
+        p1 = p1.next
+        p2 = p2.next
+    }
+
+    // Restore the list and return the result.
+    firstHalfEnd.next = reverseList(secondHalfStart)
+    return result
+}
+
+// Taken from https://leetcode.com/problems/reverse-linked-list/solution/
+private fun reverseList(head: ListNode?): ListNode? {
+    var prev: ListNode? = null
+    var curr = head
+    while (curr != null) {
+        val nextTemp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nextTemp
+    }
+    return prev
+}
+
+private fun endOfFirstHalf(head: ListNode): ListNode? {
+    var fast: ListNode? = head
+    var slow: ListNode? = head
+    while (fast!!.next != null && fast.next!!.next != null) {
+        fast = fast.next!!.next
+        slow = slow!!.next
+    }
+    return slow
 }
